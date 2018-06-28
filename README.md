802c93327126c8f51eb970db1d989ebe66e51228
Kristof Mickeleit <mickeleit.kristof@gmail.com>
2018-06-28 22:33:59 +0200

    some more documentation

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # Compound
 
-**TODO: Add description**
+Compound is a minimalistic TCP Server written in Elixir.
+## Installation 
 
-## Installation
-
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `compound` to your list of dependencies in `mix.exs`:
+Compound can be added to your project 
+by append `compound` to your list of dependencies in `mix.exs`:
 
 ```elixir
 def deps do
@@ -15,7 +14,21 @@ def deps do
 end
 ```
 
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at [https://hexdocs.pm/compound](https://hexdocs.pm/compound).
+and then running `mix deps.get` to install it.
+
+## Preparation
+
+Before you can use Compound you have to take a few steps.
+
+First you need to add one or more server configurations to your config:
+
+```elixir
+config :compound, tcp_servers: [
+  %{id: "example", port: 3001},
+  ...
+]
+```
+
+A server configuration consists of an id (as a `string`) and a port (as an `integer`).
+
 
--- mix.exs
+++ mix.exs
@@ -6,12 +6,17 @@ defmodule Compound.MixProject do
       app: :compound,
       version: "0.1.0",
       elixir: "~> 1.6",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "Compound",
+      source_url: "https://github.com/lvlick/Compound",
+      docs: docs()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger],
@@ -19,12 +24,32 @@ defmodule Compound.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:ex_doc, "~> 0.16", only: :dev, runtime: false}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp description() do
+    "Compound is a minimalistic TCP Server written in Elixir"
+  end
+
+  defp package() do
+    [
+      name: "compound",
+      files: ["lib", "mix.exs", "README.md", "LICENCE.txt"],
+      licenses: ["GNU General Public License v3.0"],
+      maintainers: ["Kristof Mickeleit"],
+      links: %{
+        "GitHub" => "https://github.com/lvlick/Compound"
+      }
+    ]
+  end
+
+  defp docs() do
+    [
+      main: "readme",
+      extras: ["README.md"]
     ]
   end
 end
